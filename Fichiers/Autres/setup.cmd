d81bff8cc4baa9e59daa4ef8c829797203d70ae6
Luckyluka17 <63603989+Luckyluka17@users.noreply.github.com>
2021-11-27 15:52:05 +0100

    Changements des fichiers

---
+++ Fichiers/Autres/setup.cmd
@@ -0,0 +1,27 @@
+@echo off
+title Installateur de CubeCraft
+cls
+color f
+echo ************************
+echo * CUBECRAFT SETUP AUTO *
+echo ************************
+echo Cliquez sur une touche pour commencer le téléchargement.
+pause >nul
+cls
+echo ************************
+echo * CUBECRAFT SETUP AUTO *
+echo ************************
+echo.
+curl https://raw.githubusercontent.com/Luckyluka17/CubeCraft-Launcher/main/Fichiers/Autres/font.ttf -o font.ttf
+cls
+echo ************************
+echo * CUBECRAFT SETUP AUTO *
+echo ************************
+echo.
+curl https://www.python.org/ftp/python/3.9.9/python-3.9.9-amd64.exe -o python.exe
+cls
+echo ************************
+echo * CUBECRAFT SETUP AUTO *
+echo ************************
+echo.
+curl 
